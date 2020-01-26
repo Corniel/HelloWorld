d26ceda731ca969f27b4447bbffea28420874cee
Corniel Nobel <mail@corniel.nl>
2020-01-26 22:17:15 +0100

    Generate an GUID with a date part.

---
+++ src/HelloWorld.UnitTests/IdGeneratorTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace HelloWorld.UnitTests
+{
+    public class IdGeneratorTest
+    {
+        [Test]
+        public void ByDate_2020Y01M26DWithSomeGuid_TheFirst4ByteShouldBeDifferent()
+        {
+            var id = IdGenerator.ByDate(new DateTime(2020,01,26), Guid.Parse("be54bc45-f368-42ba-9c4c-b79c362382c8"));
+            var expected = Guid.Parse("0b3ff833-f368-42ba-9c4c-b79c362382c8");
+            Assert.AreEqual(expected, id);
+        }
+
+        [Test]
+        public void ByDate_2456Y09M05D_MaxDate()
+        {
+            var date = new DateTime(2456, 09, 05, 23, 40, 07);
+            
+            var id = IdGenerator.ByDate(date, Guid.Parse("be54bc45-f368-42ba-9c4c-b79c362382c8"));
+            var expected = Guid.Parse("ffffffff-f368-42ba-9c4c-b79c362382c8");
+
+            Assert.AreEqual(expected, id);
+        }
+
+    }
+}
---
+++ src/HelloWorld/IdGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public static class IdGenerator
+    {
+        private const long TicksYear2000 = 630822816000000000L;
+
+        /// <summary>Gets a GUID where the first 4 byte are based on the date.</summary>
+        /// <remarks>
+        /// As long as Dates are between 2000 and 2456-09-05T23:40:07 you should be safe to go.
+        /// </remarks>
+        public static Guid ByDate() => ByDate(DateTime.UtcNow, Guid.NewGuid());
+
+        /// <summary>Gets a GUID where the first 4 byte are based on the date.</summary>
+        /// <remarks>
+        /// As long as Dates are between 2000 and 2456-09-05T23:40:07 you should be safe to go.
+        /// </remarks>
+        public static Guid ByDate(DateTime date, Guid guid)
+        {
+            var ticks = date.Ticks - TicksYear2000;
+            var prefix = BitConverter.GetBytes((uint)(ticks >> 25));
+            var bytes = guid.ToByteArray();
+            Buffer.BlockCopy(prefix, 0, bytes, 0, 4);
+            return new Guid(bytes);
+        }
+    }
+}
