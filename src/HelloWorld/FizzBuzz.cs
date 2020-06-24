4f143bb9717b107314b3a721b37d0e273a1795b9
Corniel Nobel <mail@corniel.nl>
2020-06-24 16:14:52 +0200

    My FizzBuzz.

---
+++ src/HelloWorld/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    public static class FizzBuzz
+    {
+        public static string String(int n) =>
+            n % 3 == 0
+            ? n % 5 != 0 ? "Fizz" : "FizzBuzz"
+            : n % 5 == 0 ? "Buzz" : n.ToString();
+    }
+}
