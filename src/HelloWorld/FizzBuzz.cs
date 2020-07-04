4052a12e09eecd012654957d1e1e4b128e3ba65c
Corniel Nobel <mail@corniel.nl>
2020-07-04 19:36:49 +0200

    Added FizzBuzz.N19142723();

--- src/HelloWorld/FizzBuzz.cs
+++ src/HelloWorld/FizzBuzz.cs
@@ -1,10 +1,26 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     public static class FizzBuzz
     {
+        /// <summary>Straightforward approach.</summary>
         public static string WithConditionalOperator(int n) =>
             n % 3 == 0
             ? n % 5 != 0 ? "Fizz" : "FizzBuzz"
             : n % 5 == 0 ? "Buzz" : n.ToString();
+
+        /// <remarks>
+        /// Uses the binary pattern of 19142723 (0x1241843) 
+        /// that contains the 4 flavors to switch on.
+        /// </remarks>
+        public static string N19142723(int n) =>
+            ((19142723 >> ((n % 15) * 2)) & 3) switch
+            {
+                1 => "Fizz",
+                2 => "Buzz",
+                3 => "FizzBuzz",
+                _ => n.ToString(),
+            };
     }
 }
---
+++ test/HelloWorld.UnitTests/FizzBuzzTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld.UnitTests
+{
+    public class FizzBuzzTest
+    {
+        [TestCaseSource(nameof(Records))]
+        public void WiWithConditionalOperator(int number, string expected)
+        {
+            AssertFx(expected, number, FizzBuzz.WithConditionalOperator);
+        }
+
+        [TestCaseSource(nameof(Records))]
+        public void N19142723(int number, string expected)
+        {
+            AssertFx(expected, number, FizzBuzz.N19142723);
+        }
+
+        private static void AssertFx(string expected, int number,  Func<int, string> fx)
+        {
+            Assert.AreEqual(expected, fx(number));
+        }
+
+        private static IEnumerable<object[]> Records()
+        {
+            return new Dictionary<int, string> {
+                { 00, "FizzBuzz" },
+                { 01, "1" },
+                { 02, "2" },
+                { 03, "Fizz" },
+                { 04, "4" },
+                { 05, "Buzz" },
+                { 06, "Fizz" },
+                { 07, "7" },
+                { 08, "8" },
+                { 09, "Fizz" },
+                { 10, "Buzz" },
+                { 11, "11" },
+                { 12, "Fizz" },
+                { 13, "13" },
+                { 14, "14" },
+                { 15, "FizzBuzz" },
+            }
+            .Select(kvp => new object[] { kvp.Key, kvp.Value });
+        }
+    }
+}
