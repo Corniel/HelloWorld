833aca04857df4d377bc0dc856f393fcb7221e93
Corniel Nobel <mail@corniel.nl>
2021-03-29 19:41:34 +0200

    formatting.

--- test/HelloWorld.UnitTests/JobInterviewing/Rounding_specs.cs
+++ test/HelloWorld.UnitTests/JobInterviewing/Rounding_specs.cs
@@ -4,13 +4,13 @@ namespace Rounding_specs
 {
     public class Negative_decimals
     {
-        [TestCase(123456.78, +2, 123456.78)]
-        [TestCase(123456.78, +1, 123456.8)]
-        [TestCase(123456.78, +0, 123457)]
-        [TestCase(123456.78, -1, 123460)]
-        [TestCase(123456.78, -2, 123500)]
-        [TestCase(123456.78, -3, 123000)]
-        [TestCase(123456.78, -4, 120000)]
+        [TestCase(123_456.78, +2, 123_456.78)]
+        [TestCase(123_456.78, +1, 123_456.8)]
+        [TestCase(123_456.78, +0, 123_457)]
+        [TestCase(123_456.78, -1, 123_460)]
+        [TestCase(123_456.78, -2, 123_500)]
+        [TestCase(123_456.78, -3, 123_000)]
+        [TestCase(123_456.78, -4, 120_000)]
         public void Round(decimal number, int decimals, decimal rounded)
         {
             Assert.AreEqual(rounded, Round(number, decimals));
